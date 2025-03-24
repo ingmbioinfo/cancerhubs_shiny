d630839a136652abe52bc8af398c13871eb526f9
ElisaArsuffi <elisaarsuffi07@gmail.com>
2025-03-24 10:15:44 +0100

    script gene network functions

---
+++ R/gene_network.R
@@ -0,0 +1,173 @@
+
+create_network <- function(data, original, cancer_type, int_type, gene, include_mutated = TRUE, crosses = FALSE ) {
+  
+  print(paste("Include Mutated:", include_mutated))
+  # Extract the relevant cancer data
+  cancer_data <- data[[cancer_type]]
+  
+  if (is.null(cancer_data)) {
+    stop("Invalid cancer type")
+  }
+  
+  if (int_type == "Interactors") {
+  # Find the row corresponding to the gene
+    
+    sel_data = cancer_data[["inter"]]
+    
+    selection <- ifelse(include_mutated, "mutated_interactors", "total_interactors")
+    
+    print(paste("Selected Column:", selection))
+
+    
+    interactors_gene = sel_data[sel_data$gene_list == gene, selection]
+    
+    if (!(selection %in% names(sel_data))) {
+      stop("Selected column does not exist in the dataset")
+    }
+    
+    if (length(interactors_gene) == 0) {
+      stop("Gene not found")
+    }
+    
+    # Extract interactors
+    interactors <- unlist(interactors_gene)
+    
+    print(length(interactors))
+    
+    if (length(interactors) > 50) {
+    
+    tumor_data <- original[[cancer_type]][["All_Genes"]]
+    filtered_data <- tumor_data[tumor_data$gene_list %in% interactors, ]
+    
+    # Step 2: Sort the filtered data by the "Network_Score" column in descending order
+    sorted_data <- filtered_data[order(-filtered_data$network_score), ]
+    
+    # Step 3: Extract the top 50 interactors
+    top_50 <- sorted_data[1:50, ]
+    
+    # Step 4: Match the top 50 interactors back to the original interactors' structure
+    interactors <- top_50$gene_list
+    
+    }
+  }
+  
+  else {
+    
+    
+    sel_data = cancer_data[["precog_inter"]]
+    
+    selection <- ifelse(include_mutated, "precog_mut", "precog")
+    
+    print(paste("Selected Column:", selection)) 
+    
+    interactors_gene = sel_data[sel_data$genes == gene, selection]
+    
+    if (!(selection %in% names(sel_data))) {
+      stop("Selected column does not exist in the dataset")
+    }
+    
+    if (length(interactors_gene) == 0) {
+      stop("Gene not found")
+    }
+    
+    # Extract interactors
+    interactors <- unlist(interactors_gene)
+    
+    if (length(interactors) > 50) {
+    
+    tumor_data <- original[[cancer_type]][["PRECOG"]]
+    filtered_data <- tumor_data[tumor_data$gene_list %in% interactors, ]
+    
+    #Sort the filtered data by the "Network_Score" column in descending order
+    sorted_data <- filtered_data[order(-filtered_data$network_score), ]
+    
+    #Extract the top 50 interactors
+    top_50 <- sorted_data[1:50, ]
+    
+    #Match the top 50 
+    interactors <- top_50$gene_list
+    
+    }
+    
+  }
+
+  
+  # Create network edges
+  edges <- data.frame(
+    from = gene,
+    to = interactors
+  )
+  
+  
+  if (crosses == TRUE ){
+  
+  print("Cross-interaction logic active")    
+    
+  additional_edges <- data.frame(from = character(), to = character(), stringsAsFactors = FALSE)
+  
+  for (i in seq_along(interactors)) {
+    interactor_i <- interactors[i]
+    
+    print(paste("Interactor:", interactor_i))
+    print(paste("selection",selection))
+    
+    column_name <- ifelse("gene_list" %in% colnames(sel_data), "gene_list", "genes")
+    
+    print(column_name)
+    
+    # Get interactors of interactor_i
+    interactor_data <- sel_data[sel_data[[column_name]] == interactor_i, selection]
+    
+    if (length(interactor_data) > 0) {
+      interactor_neighbors <- unlist(interactor_data)
+      shared_interactors <- intersect(interactor_neighbors, interactors)
+      
+      if (length(shared_interactors) > 0) {
+        new_edges <- data.frame(from = pmin(interactor_i, shared_interactors), 
+                                to = pmax(interactor_i, shared_interactors))
+        additional_edges <- rbind(additional_edges, new_edges)
+      }
+    }
+  }
+  
+  # Remove duplicate edges
+  additional_edges <- unique(additional_edges)
+  
+  # Combine all edges
+  edges <- unique(rbind(edges, additional_edges))
+  
+  edges <- edges[edges$from != edges$to, ]
+  
+  }
+  
+  
+  # Create graph object
+  g <- graph_from_data_frame(edges, directed = FALSE)
+  
+  # Plot the graph
+  
+ plot(g, vertex.size = 10, vertex.label.cex = 1,vertex.color =  ifelse(V(g)$name == gene, "pink", "#83C9C8"),
+      vertex.frame.color = ifelse(V(g)$name == gene, "pink", "#83C9C8"),
+      vertex.label.color= "black",vertex.label.font=2, main = paste("Top50 Interactors of", gene))
+ 
+
+ 
+ g
+}
+
+
+
+
+
+
+#retrive excel
+get_file_link <- function(dataframe, int_type, include_mutated) {
+  file_name <- paste0("https://github.com/ingmbioinfo/cancerhubs/raw/main/result/interactions_download/",
+                      dataframe, "_",
+                      ifelse(int_type == "Precog Interactors", "precog", "all_genes"),
+                      ifelse(include_mutated, "_mut", ""),
+                      ".xlsx"
+  )
+  return(file_name)
+}
+
